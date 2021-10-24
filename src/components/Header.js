4a37bbec4e7b09d30f664ccb80a837f93f647fef
RAza ALy <007razajutt@gmail.com>
2021-10-24 10:32:53 +0500

    add section for tesla 🔥

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            header components
+        </div>
+    )
+}
+
+export default Header
---
+++ src/components/Section.js
@@ -0,0 +1,99 @@
+import React from "react";
+import styled from "styled-components";
+
+const Section = ({
+  title,
+  description,
+  leftBtnText,
+  rightBtnText,
+  backgroundImg,
+}) => {
+
+  return (
+    <Wrap bg={backgroundImg}>
+        <ItemText>
+          <h1>{title}</h1>
+          <p>{description}</p>
+        </ItemText>
+
+        <Buttons>
+          <ButtonGroup>
+            <LeftButton>{leftBtnText}</LeftButton>
+            {
+            rightBtnText && <RightButton>{rightBtnText}</RightButton>
+            }
+      
+          </ButtonGroup>
+
+          <DownArrow src="/images/down-arrow.svg" />
+        </Buttons>
+    </Wrap>
+  );
+};
+
+export default Section;
+
+const Wrap = styled.div`
+  width: 100%;
+  height: 100%;
+  background-image: ${(props) => `url("/images/${props.bg}")`};
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  align-items: center;
+`;
+
+
+const ItemText = styled.div`
+  padding-top: 4%;
+  text-align: center;
+  h1 {
+    font-size: 2rem;
+    font-weight: 800;
+  }
+
+  p {
+    font-size: 1.5rem;
+    font-weight: bold;
+  }
+`;
+
+const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom: 30px;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+const LeftButton = styled.div`
+  background-color: rgba(23, 26, 32, 0.8);
+  color: #fff;
+  width: 256px;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+  margin: 8px;
+`;
+const RightButton = styled(LeftButton)`
+  background-color: #fff;
+  color: #000;
+`;
+const Buttons = styled.div``;
+
+const DownArrow = styled.img`
+  margin-top: 20px;
+  height: 40px;
+  animation: animateDown infinite 1.5s;
+  cursor: pointer;
+`;
